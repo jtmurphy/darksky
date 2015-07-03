712c871f4477bd66c366efdd08781d1cc0b1b605
John Murphy <jtmurphy@vt.edu>
2015-07-03 16:16:08 -0400

    Added documentation file

---
+++ doc.go
@@ -0,0 +1,9 @@
+/*
+	Package darksky implements a simple function that retreives 
+	weather data at a given location.
+
+	Use of this package require that you have access to a dark sky API
+	key. You can obtain an API key by creating an account at
+	https://developer.forecast.io/
+*/
+package darksky
